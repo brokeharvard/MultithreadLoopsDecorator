939c4aa14fc48a46afb478c0a71b67fd92aac7e6
brokeharvard <Colarossi@Michaels-MacBook-Pro-2.local>
2017-02-11 21:28:48 -0500

    Cleaning up the explanation (below the decorator)

--- multithreading_loop_decorator.py
+++ multithreading_loop_decorator.py
@@ -12,17 +12,21 @@ def async(function):
 		return results
 	return wrapped_function
 
-### HOW IT WORKS ###
 
 '''
+
+### WHAT IT DOES ###
+
 - Takes as its parameter a function that loops its own iterable parameter and returns iterable
-- Makes function split work of looping iterable (and doing whatever else the function does) into up to 15 
-	threads
+- Makes function split work of looping iterable (and doing whatever else the function does) into up to 15 threads
 - Once all work completed, returns map object 
-'''
+
+### HOW IT WORKS ###
+
+Just put "@async" (without the quotation marks) before the function that you want to multithread
 
 ### TO DO ###
 
-'''
 - Might be nice to add optimal max_workers parameter to change default of 15 max workers (threads)
+
 '''
